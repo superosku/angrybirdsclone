a5346abda2ed8c167a958d3d42b53be402e8e740
jaine <essi.jukkala@aalto.fi>
2013-11-05 21:36:33 +0200

    graphics

--- graphicstestmain.cc
+++ graphicstestmain.cc
@@ -8,10 +8,19 @@ int main()
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Test");
     sf::Texture bgt;
     sf::Sprite bg;
+    sf::CircleShape circle(50);
+    circle.setOrigin(50,50);
+    sf::RectangleShape rect(sf::Vector2f(100, 100));
+    sf::RectangleShape ground(sf::Vector2f(1280, 150));
+    rect.setOrigin(50,50);
+    circle.setFillColor(sf::Color(100, 250, 50));
+    rect.setFillColor(sf::Color(100, 100, 100));
+    ground.setFillColor(sf::Color(100, 250, 50));
+    ground.setPosition(0, 570);
     bgt.loadFromFile("kentta.png");
     bg.setTexture(bgt);
     Map m;
-
+    size_t c = 50;
     //ground is and 'object' too and should be created here instead of being static part of background
     
     while(window.isOpen())
@@ -25,12 +34,15 @@ int main()
 	window.clear();
 	window.draw(bg);
         //draw ground
+        window.draw(ground);
 
         std::vector<MoveableObject*> objects = m.getObjects();
       
         //TODO get type and radius/lengths of sides/etc
         for(auto& i: objects)
         {
+            size_t x = i->getX()*c + 12*c;
+            size_t y = i->getY()*(-1)*c + 8*c;
             //getType
             //getImagepath
 
@@ -48,10 +60,14 @@ int main()
             //i->getY
 	
             //set position
+            rect.setPosition(x,y);
+            rect.setRotation(i->getAngle() * -57.295);
             //draw
+            window.draw(rect);
         }     
 
 	window.display();
+        m.Step();
     }
     return 0;
 }
