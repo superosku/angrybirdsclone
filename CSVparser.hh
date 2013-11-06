b34a164182059536d998da9dedce5c50a10b6bb0
Oyni <toni.kangas@aalto.fi>
2013-11-06 15:13:57 +0200

    Added CSV-parser

---
+++ CSVparser.hh
@@ -0,0 +1,39 @@
+#ifndef CSVparser
+#define CSVparser
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> toSTR(std::string in)
+{
+  std::vector<std::string> out;
+  size_t i = 0,j = 0;
+  
+  for (auto k : in)
+  {
+    if (k == ',')
+    {
+      out.push_back(in.substr(i,(j-i)));
+      i = j+1;
+    }
+    j++;
+  }
+  out.push_back(in.substr(i,(j-i)));
+  return(out);
+}
+
+std::string toCSV(std::vector<std::string> in)
+{
+  std::string out;
+  
+  for (auto i : in)
+  {
+    out += i;
+    out += ',';
+  }
+  out.pop_back();
+  
+  return (out);
+}
+
+#endif
