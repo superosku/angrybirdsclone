6d1688dc6e56b2e1be496f247955b472e7023cf0
Teemu Mikkonen <mikkte@gmail.com>
2013-11-14 15:10:24 +0200

    Fixed setImpulse

--- moveable.hh
+++ moveable.hh
@@ -65,8 +65,8 @@ class MoveableObject
 
     void setImpulse(float x, float y) {
       //body->ApplyLinearImpulse(b2Vec2(x,y), body->GetWorldCenter(),true);
-      //body->ApplyLinearImpulse(b2Vec2(x,y), body->GetWorldCenter());
-      body->SetLinearVelocity(b2Vec2(x,y));
+      body->ApplyLinearImpulse(b2Vec2(x,y), body->GetWorldCenter());
+      //body->SetLinearVelocity(b2Vec2(x,y));
     }
     MoveableObject::Type getType() {
       return type;
