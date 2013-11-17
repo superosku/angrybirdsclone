7ddc64187d82e9be97db1d46cee55c9e1937d15a
jaine <essi.jukkala@aalto.fi>
2013-11-17 20:26:35 +0200

    added background

--- graphics.hh
+++ graphics.hh
@@ -16,8 +16,9 @@
 class Graphics {
   private:
     sf::RenderWindow window;
-    //sf::Texture bgt;
-    //sf::Sprite bg;
+    sf::View view;
+    sf::Texture bgt;
+    sf::Sprite bg;
     sf::Font font;
     sf::Texture kemma;
     sf::Texture kone;
@@ -47,9 +48,8 @@ class Graphics {
     Graphics() : window(sf::VideoMode(1280, 720), "Game jou", sf::Style::Default/*, settings*/) {
       m = new Map();
       window.setFramerateLimit(60);
-      //bgt.loadFromFile("kentta.png");
-      //bg.setTexture(bgt);
-      if (!font.loadFromFile("QuinzeNarrow.ttf")) {
+      view.reset(sf::FloatRect(0, 0, 1280, 720));
+      if(!font.loadFromFile("QuinzeNarrow.ttf")) {
         //Throw error or something
       }
       if(!kemma.loadFromFile("kemma.png")){
@@ -82,7 +82,9 @@ class Graphics {
       if(!tnt.loadFromFile("tnt.png")){
           //Throw error
       }
-
+      if(!bgt.loadFromFile("bg.png")){
+          //Throw error
+      }
       
       kemma.setSmooth(true);
       kone.setSmooth(true);
@@ -94,10 +96,14 @@ class Graphics {
       tile3.setSmooth(true);
       tile4.setSmooth(true);
       tnt.setSmooth(true);
+      bgt.setSmooth(true);
       //settings.antialiasingLevel = 8;
-      c = 35;
+      bg.setTexture(bgt);
+      window.setView(view);
+
+      c = 30;
       cam_x = 5;
-      cam_y = 5;
+      cam_y = 9.4;
       shoot_aiming = 0;
     }
     int convertX(float x) {
@@ -119,6 +125,17 @@ class Graphics {
           if (event.type == sf::Event::Closed) {
             window.close();
           }
+          if(event.type == sf::Event::Resized)
+          {
+              sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+              window.setView(sf::View(visibleArea));
+          }
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+              view.move(25,0);
+          }
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+              view.move(-25,0);
+          }
           if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
             // Check if we are pressing near catapult
             if (event.mouseButton.x < convertX(m->getCatapultX()+0.5) and event.mouseButton.x > convertX(m->getCatapultX()-0.5) and
@@ -138,7 +155,7 @@ class Graphics {
                    (convertX(m->getCatapultX()) - event.mouseButton.x)/10.0,
                   -(convertY(m->getCatapultY()) - event.mouseButton.y)/10.0);
               shoot_aiming = 0;
-              }
+              } 
           }
           if (event.type == sf::Event::KeyPressed) {
             if (event.key.code == sf::Keyboard::R) {
@@ -147,17 +164,19 @@ class Graphics {
             }
           }
         }
-        window.clear(sf::Color(160,160,255));
+        window.clear(/*sf::Color(160,160,255)*/);
 
+        window.setView(view);
+        window.draw(bg);
         // Drawing the ground
-        sf::RectangleShape ground(sf::Vector2f(1280, 720 - convertY(0)));
+        sf::RectangleShape ground(sf::Vector2f(2560, 720 - convertY(0)));
         ground.setPosition(0, convertY(0));
-        ground.setFillColor(sf::Color(100,255,100));
+        ground.setFillColor(sf::Color(42,100,48));
         window.draw(ground);
 
         // Drawing the catapult
         sf::CircleShape catapult_bg(convertDistance(0.5));
-        catapult_bg.setFillColor(sf::Color(100,100,200));
+        catapult_bg.setFillColor(sf::Color(47,60,74));
         catapult_bg.setOrigin(convertDistance(0.5), convertDistance(0.5));
         catapult_bg.setPosition(convertX(m->getCatapultX()), convertY(m->getCatapultY()));
 
