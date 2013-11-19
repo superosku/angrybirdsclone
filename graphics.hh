f41d9602f91e02afa4505f7999f00124171ee77a
Teemu Mikkonen <mikkte@gmail.com>
2013-11-19 23:20:11 +0200

    Fixed window resize problem

--- graphics.hh
+++ graphics.hh
@@ -274,7 +274,8 @@ class Graphics {
 
         catapult.setOrigin(convertDistance(0.5), convertDistance(0.5));
         if (shoot_aiming) 
-          catapult.setPosition((sf::Mouse::getPosition(window).x+gx)*1280/window_w, (sf::Mouse::getPosition(window).y+gy)*720/window_h);
+          catapult.setPosition((sf::Mouse::getPosition(window).x)*1280/window_w-gx, (sf::Mouse::getPosition(window).y)*720/window_h-gy);
+          //catapult.setPosition((sf::Mouse::getPosition(window).x)*1280/window_w, (sf::Mouse::getPosition(window).y)*720/window_h);
         else
           //catapult.setPosition(convertX(m->getCatapultX()), convertY(m->getCatapultY()));
           catapult.setPosition(catapult_x, catapult_y);
